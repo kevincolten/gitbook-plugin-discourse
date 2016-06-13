7c244a60fc7b7887cdfd68eec49f5e76db2de2f1
Emmanuel Raviart <emmanuel@raviart.com>
2016-06-13 18:32:29 +0200

    Remove config from page.change event.

--- book/plugin.js
+++ book/plugin.js
@@ -2,13 +2,7 @@ require([
   "gitbook",
   "jQuery"
 ], function(gitbook, $) {
-  function embedDiscourse(config) {
-    config['discourse-external-integration'] = config['discourse-external-integration'] || {};
-    DiscourseEmbed = {
-      discourseUrl: config['discourse-external-integration'].discourseUrl,
-      discourseEmbedUrl: window.location.href
-    };
-
+  function embedDiscourse() {
     // The only way to re-execute Discourse embedding script seems to be to re-insert it.
     $("#discourse-comments").remove();
     $("#discourse-embed-script").remove();
@@ -18,14 +12,19 @@ require([
     $("head").append($("<script>", {
       "async": true,
       "id": "discourse-embed-script",
+      "src": DiscourseEmbed.discourseUrl + 'javascripts/embed.js';
       "type": "text/javascript"
     }));
   }
 
   gitbook.events.bind("start", function (e, config) {
+    config['discourse-external-integration'] = config['discourse-external-integration'] || {};
+    DiscourseEmbed = {
+      discourseUrl: config['discourse-external-integration'].discourseUrl,
+      discourseEmbedUrl: window.location.href
+    };
     embedDiscourse(config);
   });
-  gitbook.events.bind("page.change", function (e, config) {
-    embedDiscourse(config);
-  });
+
+  gitbook.events.bind("page.change", embedDiscourse);
 });
